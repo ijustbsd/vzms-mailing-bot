85f31a83d2f8825ab8f75e0fe54dee34536b10f6
Sergey Baboshin <ijustbsd@gmail.com>
2021-12-05 15:26:30 +0300

    chore(tests): add tests

--- app/__main__.py
+++ app/__main__.py
@@ -1,62 +1,7 @@
-import asyncio
-import io
-import logging
-from pathlib import Path
+from aiogram import executor
 
-import aiohttp
-from aiogram import Bot, Dispatcher, executor, types
-
-from .config import settings
-
-logging.basicConfig(level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-bot = Bot(token=settings.TG_BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot)
-
-
-with Path(__file__).parent.joinpath("templates/message.html").open() as f:
-    email_template = f.read()
-
-
-@dp.channel_post_handler(
-    hashtags=[settings.TG_HASTAG],
-    chat_id=settings.TG_CHAT_ID,
-    content_types=[types.ContentType.TEXT, types.ContentType.DOCUMENT],
-)
-async def send_email(message: types.Message):
-    # удаляем хештег и переносы строк в конце сообщения
-    email_text = message.html_text.replace(f"#{settings.TG_HASTAG}", "").rstrip("\n")
-
-    async with aiohttp.ClientSession() as session:
-        data = aiohttp.FormData()
-        data.add_field("from", settings.EMAIL_FROM)
-        data.add_field("to", settings.EMAIL_TO)
-        data.add_field("subject", settings.EMAIL_SUBJECT)
-        data.add_field(
-            "html",
-            email_template.replace("%message%", email_text).replace("\n", "<br>"),
-        )
-        if message.document is not None:
-            data.add_field(
-                "attachment",
-                await message.document.download(destination_file=io.BytesIO()),
-                filename=message.document.file_name,
-            )
-        params = {
-            "auth": aiohttp.BasicAuth("api", settings.MAILGUN_API_KEY),
-            "data": data,
-        }
-        for _ in range(5):
-            resp = await session.post(
-                f"https://api.mailgun.net/v3/{settings.EMAIL_DOMAIN}/messages", **params
-            )
-            if resp.status == 200:
-                break
-            logger.warning("Mailgun response status: %s", resp.status)
-            await asyncio.sleep(3)
-
+from .bot import create_bot
 
 if __name__ == "__main__":
+    dp = create_bot()
     executor.start_polling(dp)
---
+++ tests/test_app.py
@@ -0,0 +1,53 @@
+from unittest.mock import AsyncMock
+
+import pytest
+from aioresponses import CallbackResult, aioresponses
+from app.bot import send_email
+from yarl import URL
+
+
+@pytest.mark.asyncio
+async def test_send_mail():
+    def callback(url, **kwargs):
+        assert url == URL("https://api.mailgun.net/v3/test_email_domain/messages")
+
+        request_auth = kwargs["auth"]
+        assert request_auth.login == "api"
+        assert request_auth.password == "test_mailgun_api_key"
+
+        request_data = kwargs["data"]
+        data_fields = {field[0]["name"]: field[2] for field in request_data._fields}
+
+        assert len(data_fields) == 5
+
+        assert data_fields["from"] == "test_email_from"
+        assert data_fields["to"] == "test_email_to"
+        assert data_fields["subject"] == "test_email_subject"
+        assert data_fields["html"] == (
+            "<html><br>Бла-бла-бла<br><br>"
+            "<i>Это сообщение было отправлено автоматически. "
+            "Пожалуйста, не отвечайте на него! Почта для связи с оргкомитетом: "
+            '<a href="mailto:vzms@mail.ru">vzms@mail.ru</a>. '
+            "Чтобы отписаться от новостных сообщений перейдите по "
+            '<a href="%mailing_list_unsubscribe_url%">ссылке</a>.</i>'
+            "<br></html><br>"
+        )
+        assert data_fields["attachment"] == b"\xDE\xAD\xF0\x0D"
+
+        return CallbackResult(status=200)
+
+    async def download(destination_file):
+        return b"\xDE\xAD\xF0\x0D"
+
+    with aioresponses() as mock:
+        mock.post(
+            "https://api.mailgun.net/v3/test_email_domain/messages",
+            callback=callback,
+        )
+
+        document = AsyncMock(file_name="test_file", download=download)
+        message = AsyncMock(html_text="Бла-бла-бла\n#рассылка", document=document)
+
+        await send_email(message=message)
+
+        assert len(mock.requests) == 1
